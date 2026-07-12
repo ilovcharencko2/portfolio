30f4e850bc18be156b3316b0424a75cee8ac1477
ilovcharencko2 <ilovcharencko2@gmail.com>
2026-07-12 14:54:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-// Ждем загрузки DOM
 document.addEventListener("DOMContentLoaded", (event) => {
-    // Используем GSAP для плавного появления всех секций с классом animate-item
     gsap.from(".animate-item", {
-        y: 30,           // Элементы выезжают снизу (на 30px)
-        opacity: 0,      // Начинают с нулевой прозрачности
-        duration: 0.8,   // Длительность анимации для каждого элемента
-        stagger: 0.2,    // Задержка между появлениями элементов
-        ease: "power2.out" // Плавное замедление в конце
+        y: 30,
+        opacity: 0,
+        duration: 0.8,
+        stagger: 0.2,
+        ease: "power2.out"
     });
 });
